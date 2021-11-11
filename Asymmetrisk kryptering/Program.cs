abb43d75724598195f605b10aad76b3b7fe326b1
Marc Andersen <marchot1999@gmail.com>
2021-11-11 09:19:42 +0100

    Add project files.

---
+++ Asymmetrisk kryptering/Program.cs
@@ -0,0 +1,30 @@
+﻿using Asymmetrisk_kryptering;
+using System.Text;
+
+RSA rsa = new RSA();
+
+WriteToXML();
+Console.ReadLine();
+
+
+void WriteToXML()
+{
+    string textToEncrypt = "Hello World";
+
+    //path for keys to be stored
+    string publicKeyPath = @"C:\RSAKeys\publicKey.xml";
+    string privateKeyPath = @"C:\RSAKeys\privateKey.xml";
+
+    //Assign new keys to the path   
+    rsa.AssignNewKey(publicKeyPath, privateKeyPath);
+
+    //Encrypt and decrypt or text
+    byte[] encrypted = rsa.Encrypt(publicKeyPath, Encoding.UTF8.GetBytes(textToEncrypt));
+    byte[] decrypted = rsa.Decrypt(privateKeyPath, encrypted);
+
+    //Write or data to the console.
+    Console.WriteLine($"Key stored in XML");
+    Console.WriteLine($"Plain text: {textToEncrypt}");
+    Console.WriteLine($"Encrypted: {Convert.ToBase64String(encrypted)}");
+    Console.WriteLine($"Decrypted: {Encoding.Default.GetString(decrypted)}");
+}
---
+++ Asymmetrisk kryptering/RSA.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Asymmetrisk_kryptering
+{
+    internal class RSA
+    {
+
+        //Generate public and private key. To asigned paths
+        public void AssignNewKey(string publicKeyPath, string privateKeyPath)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                if (File.Exists(privateKeyPath))
+                {
+                    File.Delete(privateKeyPath);
+                }
+
+                if (File.Exists(publicKeyPath))
+                {
+                    File.Delete(publicKeyPath);
+                }
+
+                var publicKeyfolder = Path.GetDirectoryName(publicKeyPath);
+                var privateKeyfolder = Path.GetDirectoryName(privateKeyPath);
+
+                if (!Directory.Exists(publicKeyfolder))
+                {
+                    Directory.CreateDirectory(publicKeyfolder);
+                }
+
+                if (!Directory.Exists(privateKeyfolder))
+                {
+                    Directory.CreateDirectory(privateKeyfolder);
+                }
+
+                File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+                File.WriteAllText(privateKeyPath, rsa.ToXmlString(true));
+            }
+        }
+
+        //Encrypt or message with the public key
+        public byte[] Encrypt(string publicKeyPath, byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+
+                cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+            }
+
+            return cipherbytes;
+        }
+
+        //Decrypt the encrypted message with the private key
+        public byte[] Decrypt(string privateKeyPath, byte[] dataToEncrypt)
+        {
+            byte[] plain;
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+                plain = rsa.Decrypt(dataToEncrypt, false);
+            }
+
+            return plain;
+        }
+    }
+}
